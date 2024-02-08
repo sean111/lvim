f833737ee4bdf72b6c2fda8475e9d42c5b710006
Sean Burke <sean111@gmail.com>
2024-02-07 20:21:21 -0500

    added just & outline plugins Added some more default options to eventually tweak from obsidian and some key bindings

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,9 @@
+return {
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlienOpen" },
+  keys = {
+    { "<leader>on", "<cmd>Outline<cr>", desc = "Toggle Outline" },
+  },
+  opts = {},
+}
