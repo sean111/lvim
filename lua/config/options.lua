b5f51fabde8f6dfe0636bd3ead0728a48475af50
Sean Burke <sean111@gmail.com>
2024-02-25 12:18:29 -0500

    Added some options for stuff like shared clipbaord, true color and conceallevel

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt_local.conceallevel = 1
+local opt = vim.opt
+
+opt.conceallevel = 1
+opt.termguicolors = true
+opt.clipboard = "unnamed,unnamedplus"
