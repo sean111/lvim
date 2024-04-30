2ba9ccfb34c4f7bad165fa4c8f0e9e96026653ef
Sean Burke <sean111@gmail.com>
2024-04-30 00:50:55 -0400

    added cyberdream color scheme as the default

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -14,10 +14,27 @@ return {
     end,
   },
   {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        -- Recommended - see "Configuring" below for more config options
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = true,
+        terminal_colors = true,
+      })
+      -- vim.cmd("colorscheme cyberdream") -- set the colorscheme
+    end,
+  },
+  {
     {
       "LazyVim/LazyVim",
       opts = {
-        colorscheme = "nordfox",
+        -- colorscheme = "nordfox",
+        colorscheme = "cyberdream",
       },
     },
   },
