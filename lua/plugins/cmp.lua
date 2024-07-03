9ee707262fec6624d70f6cc5887a53137e5722d2
Sean Burke <sean111@gmail.com>
2024-07-02 22:48:30 -0400

    Update lazy Add in some CMP options Switch back to nordfox theme

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,5 +7,49 @@ return {
 
     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { {} }))
     opts.experimental.ghost_text = true
+
+    cmp.setup({
+      window = {
+        completion = {
+          winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+          col_offset = -3,
+          side_padding = 0,
+        },
+      },
+      formatting = {
+        expandable_indicator = true,
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, item)
+          local kind = item.kind
+          local kind_hl_group = ("CmpItemKind%s"):format(kind)
+
+          item.kind_hl_group = ("%sIcon"):format(kind_hl_group)
+          item.kind = (" %s "):format(COMPLETION_KIND[kind].icon)
+
+          local source = entry.source.name
+          if source == "nvim_lsp" or source == "path" then
+            item.menu_hl_group = kind_hl_group
+          else
+            item.menu_hl_group = "Comment"
+          end
+          item.menu = kind
+
+          if source == "buffer" then
+            item.menu_hl_group = nil
+            item.menu = nil
+          end
+
+          local half_win_width = math.floor(vim.api.nvim_win_get_width(0) * 0.5)
+          if vim.api.nvim_strwidth(item.abbr) > half_win_width then
+            item.abbr = ("%s…"):format(item.abbr:sub(1, half_win_width))
+          end
+
+          if item.menu then -- Add exta space to visually differentiate `abbr` and `menu`
+            item.abbr = ("%s "):format(item.abbr)
+          end
+          return item
+        end,
+      },
+    })
   end,
 }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,28 +13,28 @@ return {
       })
     end,
   },
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("cyberdream").setup({
-        -- Recommended - see "Configuring" below for more config options
-        transparent = true,
-        italic_comments = true,
-        hide_fillchars = true,
-        borderless_telescope = true,
-        terminal_colors = true,
-      })
-      -- vim.cmd("colorscheme cyberdream") -- set the colorscheme
-    end,
-  },
+  -- {
+  --   "scottmckendry/cyberdream.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("cyberdream").setup({
+  --       -- Recommended - see "Configuring" below for more config options
+  --       transparent = false,
+  --       italic_comments = true,
+  --       hide_fillchars = true,
+  --       borderless_telescope = true,
+  --       terminal_colors = true,
+  --     })
+  --     -- vim.cmd("colorscheme cyberdream") -- set the colorscheme
+  --   end,
+  -- },
   {
     {
       "LazyVim/LazyVim",
       opts = {
-        -- colorscheme = "nordfox",
-        colorscheme = "cyberdream",
+        colorscheme = "nordfox",
+        -- colorscheme = "cyberdream",
       },
     },
   },
